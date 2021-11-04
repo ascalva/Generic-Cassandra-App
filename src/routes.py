fc192286f7ddfc8055c0a3e6a92e2fb9484e714d
Alberto Serrano <ascalva@gmail.com>
2021-11-04 18:02:02 -0400

    Added basic routes for creating and returning users/movies

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, render_template, request
 from src   import app
 
 
@@ -13,33 +13,47 @@ def index() :
     return "work in progress"
 
 
-@api.route("/createTable", methods=['GET'], strict_slashes=False)
-def create_table() :
-    app.db.createTableUser()
-    app.db.createTableMovie()
-    return "success"
-
-
-@api.route("/checkTable", methods=['GET'], strict_slashes=False)
-def check_table() :
-    name = "user"
-    res = app.db.tableExists(name)
-    return f"Table '{name}' exists: {res}"
+# @api.route("/checkTable", methods=['GET'], strict_slashes=False)
+# def check_table() :
+#     name = "user"
+#     res = app.db.tableExists(name)
+#     return f"Table '{name}' exists: {res}"
 
 
 @api.route("/new/<fname>/<lname>/<role>", methods=['GET'], strict_slashes=False)
 def new_user(fname, lname, role) :
-    app.db.createUser(fname, lname, role)
+    app.db.createPerson(fname, lname, role)
     return "success"
 
 
 @api.route("/getUsers", methods=['GET'], strict_slashes=False)
 def get_users() :
-    def format_q(elt) :
-        return f"{elt.user_id} : {elt.fname} {elt.lname}"
+    out_dic = {}
 
-    res = app.db.getTable("person")
+    for p in app.db.getTable("person") :
+        out_dic[str(p.user_id)] = {"name" : f"{p.fname} {p.lname}", "role" : p.role}
 
-    return jsonify([format_q(i) for i in res])
+    return jsonify(out_dic)
 
+
+@api.route("/getMovies", methods=['GET'], strict_slashes=False)
+def get_movies() :
+    out_dic = {}
+    for m in app.db.getTable("movie") :
+        out_dic[m.title] = {"year" : m.year, "director" : m.director}
+
+    return jsonify(out_dic)
+
+
+@api.route("/createMovie", methods=['GET', 'POST'], strict_slashes=False)
+def createMovie() :
+    if request.method == "POST" :
+        movie_name = request.form['movie_name']
+        director   = request.form['director']
+        year       = int(request.form['year'])
+
+        app.db.createMovie(movie_name, director, year)
+        return "success"
+
+    return render_template("create_movie.html")
 
